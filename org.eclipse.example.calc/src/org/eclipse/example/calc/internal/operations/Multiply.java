8bc1a04ec0e4a210d1c7c2996f02a09769d4ae20
v088961 <v088961@DARWINDE-004864.de.top.com>
2016-06-08 16:24:24 +0200

    Added JavaDoc On Multiply

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Multiply.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Multiply.java
@@ -1,7 +1,11 @@
 package org.eclipse.example.calc.internal.operations;
 
 import org.eclipse.example.calc.BinaryOperation;
-
+/**
+ * 
+ * @author v088961
+ *
+ */
 public class Multiply  extends AbstractOperation implements BinaryOperation {
 
 	@Override
